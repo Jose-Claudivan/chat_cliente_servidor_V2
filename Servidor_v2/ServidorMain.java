fdedc8b1534b398665bb4a9bc7aab6af14ffceaf
Jose-Claudivan <claudivan0103@gmail.com>
2020-05-15 00:22:36 -0300

    Thread de tratamento de clientes no ServidorMain e TrataCliente

--- Servidor_v2/ServidorMain.java
+++ Servidor_v2/ServidorMain.java
@@ -13,19 +13,55 @@
 package servidor_v2;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.*;
+import java.util.ArrayList;
 
 public class ServidorMain {
 //  metodo principal que faz a chamada do metodo iniciar()
 //  atraves do objeto servidor
-    public static void main(String[] args) {
-
-      ServidorModerador servidor = new ServidorModerador();
+    public static void main(String[] args) throws IOException {
+      //inicia o servidor
+      new ServidorMain(12345).executa();
+     /* ServidorModerador servidor = new ServidorModerador();
       try {
         servidor.iniciar();
       } catch (Exception ex) {
          // Logger.getLogger(Ap1_Servidor.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }    
+        }*/
+    }
+    private int porta;
+    private List<PrintStream>clientes;
+
+    public ServidorMain(int porta) {
+      this.porta = porta;
+      this.clientes = new ArrayList<PrintStream>();
+    }
+    public void executa() throws IOException {
+      ServerSocket servidor = new ServerSocket(this.porta);
+      System.out.println("Porta aberta: -->" + porta);
+
+      while(true){
+        //aceita um cliente
+        Socket cliente = servidor.accept();
+        System.out.println("Nova conexão com o cliente " +     
+                cliente.getInetAddress().getHostAddress());
+        //adiciona saida do cliente a lista
+        PrintStream ps = new PrintStream(cliente.getOutputStream());
+        this.clientes.add(ps);
+        //cria tratador de cliente numa nova therad
+        TrataCliente tc = new TrataCliente(cliente.getInputStream(),this);
+        new Thread(tc).start();
+      }
+    }
+    public void distribuiMensagem(String msg) {
+      // envia msg para todo mundo
+      for (PrintStream cliente : this.clientes) {
+          cliente.println(msg);
+      }
+  }
 }
